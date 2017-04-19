ef91fc87d728b132595011e856148c85d6142f2c
Michael Chapman <submariner100@gmail.com>
2017-04-19 21:15:11 +0100

    final draft

--- Project14/GameScene.swift
+++ Project14/GameScene.swift
@@ -17,6 +17,7 @@ import GameplayKit
 
 class GameScene: SKScene {
 
+     var numRounds = 0
      var popupTime = 0.85
      var slots = [WhackSlot]()
      var gameScore: SKLabelNode!
@@ -36,6 +37,24 @@ class GameScene: SKScene {
      }
      
      func createEnemy() {
+     
+          numRounds += 1
+          
+          if numRounds >= 30 {
+               for slot in slots {
+                    slot.hide()
+               }
+               
+               let gameOver = SKSpriteNode(imageNamed: "gameOver")
+               gameOver.position = CGPoint(x: 512, y: 384)
+               gameOver.zPosition = 1
+               addChild(gameOver)
+               
+               return
+          }
+     
+     
+     
           popupTime *= 0.991
           
           slots = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: slots) as! [WhackSlot]
@@ -80,15 +99,50 @@ class GameScene: SKScene {
           
           DispatchQueue.main.asyncAfter(deadline: .now() + 1) { [unowned self] in
                self.createEnemy()
-        
+               
           }
-     
+          
      }
      
-     
-        
      override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-     
+          if let touch = touches.first {
+               let location = touch.location(in: self)
+               let tappedNodes = nodes(at: location)
+               
+               
+               for node in tappedNodes {
+                    if node.name == "charFriend" {
+                         // they should'nt have whacked this penguin
+                         
+                    let whackSlot = node.parent!.parent as! WhackSlot
+                    if !whackSlot.isVisible { continue }
+                    if whackSlot.isHit { continue }
+                    
+                    whackSlot.hit()
+                    score -= 5
+                    
+                    run(SKAction.playSoundFileNamed("whackBad.caf", waitForCompletion: false))
+                    
+                    
+                    
+                    } else if node.name == "charEnemy" {
+                         // they should have whacked this one
+                    
+                    let whackSlot = node.parent!.parent as! WhackSlot
+                    if !whackSlot.isVisible { continue }
+                    if whackSlot.isHit { continue }
+                    
+                    whackSlot.charNode.xScale = 0.85
+                    whackSlot.charNode.yScale = 0.85
+                    
+                    whackSlot.hit()
+                    score += 1
+                    
+                    run(SKAction.playSoundFileNamed("whack.caf", waitForCompletion: false))
+                    
+                    }
+               }
+          }
      }
         
    }
--- Project14/WhackSlot.swift
+++ Project14/WhackSlot.swift
@@ -41,6 +41,17 @@ class WhackSlot: SKNode {
                
      }
      
+     func hit() {
+          isHit = true
+          
+          let delay = SKAction.wait(forDuration: 0.25)
+          let hide = SKAction.moveBy(x: 0, y: -80, duration: 0.5)
+          let notVisible = SKAction.run {
+               [unowned self] in self.isVisible = false }
+          charNode.run(SKAction.sequence([delay, hide, notVisible]))
+       
+     }
+     
      func hide() {
           
           if !isVisible { return }
@@ -55,6 +66,9 @@ class WhackSlot: SKNode {
      
      func show(hideTime: Double) {
           if isVisible { return }
+          
+          charNode.xScale = 1
+          charNode.yScale = 1
           
           charNode.run(SKAction.moveBy(x: 0, y: 80, duration: 0.05))
           isVisible = true
